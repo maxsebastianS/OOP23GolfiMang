d028752d650925da03aa4617cc7b306613a29877
Max Sebastian Segerkrantz <sebastiansegerkrantz@gmail.com>
2023-03-11 21:31:33 +0200

    Lisasin 5 rada

--- src/Golfimäng.java
+++ src/Golfimäng.java
@@ -1,5 +1,9 @@
-public class Main {
+public class Golfimäng {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Rada rada1 = new Rada(1,456,5);
+        Rada rada2 = new Rada(2,298,4);
+        Rada rada3 = new Rada(3,310,4);
+        Rada rada4 = new Rada(4,380,4);
+        Rada rada5 = new Rada(5,140,3);
     }
 }
